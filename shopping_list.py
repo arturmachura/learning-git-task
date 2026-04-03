fbeff0fdb2a1c4896f577f0dcec0e53270558e78
Artur Machura <artur.machura@soflab.pl>
2026-04-03 11:57:49 +0200

    Add shopping list

--- shopping_list.py
+++ shopping_list.py
@@ -3,3 +3,9 @@ shopping = {
     "lidl": ["jabłka", "jogurt"],
     "apteka": ["witaminy", "plaster"]
 }
+
+for store, products in shopping.items():
+    print(f"Idę do {store.upper()} i kupuję tam {', '.join(products).upper()}.")
+
+total_products = sum(len(products) for products in shopping.values())
+print(f"W sumie kupuję {total_products} produktów.")
